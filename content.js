f71e5b46c70d2ac8027ce1a11676e0b595cc976c
皮卡啾 <104876388+pikachu2022@users.noreply.github.com>
2026-03-31 18:40:50 +0800

    Add files via upload

---
+++ content.js
@@ -0,0 +1,373 @@
+//紅綠燈
+var k3 = function(){
+	var kk3 = setInterval(function(){
+		if(document.getElementsByClassName("h-8 w-full next-color-box text-black-dark display-font justify-center items-center font-bold text-sm uppercase relative")[0] !== undefined){
+			if(document.getElementsByClassName("h-8 w-full next-color-box text-black-dark display-font justify-center items-center font-bold text-sm uppercase relative")[0].getElementsByTagName('span')[0].textContent === '!'){
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[2].style.display = 'block';
+				const rect = document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[2].getBoundingClientRect();
+				const x = rect.left + rect.width / 2;
+				const y = rect.top + rect.height / 2;
+				chrome.runtime.sendMessage({ x, y });
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[1].style.display = 'none';
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[0].style.display = 'none';
+			}
+			else if(document.getElementsByClassName("h-8 w-full next-color-box text-black-dark display-font justify-center items-center font-bold text-sm uppercase relative")[0].getElementsByTagName('span')[0].textContent === '@'){		
+				const rect = document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[1].getBoundingClientRect();
+				const x = rect.left + rect.width / 2;
+				const y = rect.top + rect.height / 2;
+				chrome.runtime.sendMessage({ x, y });
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[1].style.display = 'block';
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[0].style.display = 'none';
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[2].style.display = 'none';
+			}
+			else{
+				const rect = document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[0].getBoundingClientRect();
+				const x = rect.left + rect.width / 2;
+				const y = rect.top + rect.height / 2;
+				chrome.runtime.sendMessage({ x, y });
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[0].style.display = 'block';
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[2].style.display = 'none';
+				//document.getElementsByClassName("flex-row w-full mb-16 justify-between opacity-90")[0].children[1].style.display = 'none';
+			}
+		}
+		else{
+			clearInterval(kk3);
+			k1();
+		}
+	}, 1);
+}
+//神經電路覆寫 連連看
+var k4 = function(){
+	let p = document.getElementsByClassName("absolute transform -translate-x-1/2 -translate-y-1/2");
+	let a = '';
+	let aa = new Array(p.length);
+	for(let i = 0; i < p.length; i+=1){
+		setTimeout(function(){
+			const rect = p[i].getBoundingClientRect();
+			const x = rect.left + rect.width / 2;
+			const y = rect.top + rect.height / 2;
+			chrome.runtime.sendMessage({ x, y });
+			a += p[i].textContent[0];
+		}, 100*i);
+	}
+	setTimeout(function(){
+		for(let i = 0; i < p.length; i+=1){
+			aa[a[i]-1] = i;
+		}
+		const rect = p[0].getBoundingClientRect();
+		const x = rect.left + rect.width / 2;
+		const y = rect.top + rect.height / 2;
+		chrome.runtime.sendMessage({ x, y });
+		//alert(aa);
+		for(let i = 0; i < p.length; i+=1){
+			setTimeout(function(){
+				const rect = p[parseInt(aa[i])].getBoundingClientRect();
+				const x = rect.left + rect.width / 2;
+				const y = rect.top + rect.height / 2;
+				chrome.runtime.sendMessage({ x, y });
+			}, 500+i*100);
+		}
+	}, 500);
+	setTimeout(function(){
+		k1();
+	}, 1000*p.length);
+}
+//攔截有效的數據封包 音遊
+var k5 = function(){
+	var kk5 = setInterval(function(){
+		if(document.getElementsByClassName("text-sm opacity-80 font-mono tracking-wide")[0] !== undefined && /攔截有效的數據封包/.test(document.getElementsByClassName("text-sm opacity-80 font-mono tracking-wide")[0].textContent)){
+			if(document.getElementsByClassName("absolute left-1/2 cursor-pointer -translate-x-1/2 h-16 w-16 pointer-events-auto border-2 flex items-center justify-center text-2xl backdrop-blur-sm bg-black border-secondary shadow-[0_0_20px_rgba(0,255,255,0.15),inset_0_0_10px_rgba(0,255,255,0.1)]")[0] != undefined){
+				const rect = document.getElementsByClassName("absolute left-1/2 cursor-pointer -translate-x-1/2 h-16 w-16 pointer-events-auto border-2 flex items-center justify-center text-2xl backdrop-blur-sm bg-black border-secondary shadow-[0_0_20px_rgba(0,255,255,0.15),inset_0_0_10px_rgba(0,255,255,0.1)]")[0].getBoundingClientRect();
+				const x = rect.left + rect.width / 2;
+				const y = rect.top + rect.height / 2;
+				chrome.runtime.sendMessage({ x, y });
+			}
+		}
+		else{
+			clearInterval(kk5);
+			k1();
+		}
+	}, 500);
+}
+//document.getElementsByClassName("text-5xl font-mono relative inline-flex items-center justify-center p-6 bg-emerald-950/50 text-emerald-400")[0].textContent
+//document.getElementsByClassName("cursor-pointer text-3xl font-mono relative p-4 transition-colors duration-200 hover:bg-emerald-900/50 text-emerald-800")[].textContent
+//解密 神經數據.exe 對圖片
+var k6 = function(){
+	var kk6 = setInterval(function(){
+		if(document.getElementsByClassName("text-xl font-mono font-bold inline-block text-emerald-400")[0] !== undefined && /解密 神經數據.exe/.test(document.getElementsByClassName("text-xl font-mono font-bold inline-block text-emerald-400")[0].textContent)){
+			let a = document.getElementsByClassName("text-5xl font-mono relative inline-flex items-center justify-center p-6 bg-emerald-950/50 text-emerald-400")[0].textContent;
+			let b = document.getElementsByClassName("cursor-pointer text-3xl font-mono relative p-4 transition-colors duration-200 hover:bg-emerald-900/50 text-emerald-800");
+			for(var i = 0; i < b.length; i+=1){
+				if(a==b[i].textContent){
+					const rect = b[i].getBoundingClientRect();
+					const x = rect.left + rect.width / 2;
+					const y = rect.top + rect.height / 2;
+					chrome.runtime.sendMessage({ x, y });
+					break;
+				}
+			}
+		}
+		else{
+			clearInterval(kk6);
+			k1();
+		}
+	}, 1);
+}
+//記憶庫覆寫 九宮格
+var k7 = function(){
+	let a = document.getElementsByClassName("grid grid-cols-3 gap-2 w-full aspect-square mb-4 p-4 rounded-lg bg-black/20 backdrop-blur-sm border border-primary/20")[0].textContent;
+	var kk7 = setInterval(function(){
+		if(document.getElementsByClassName("text-2xl font-semibold relative display-font text-secondary")[0] !== undefined && /記憶庫覆寫/.test(document.getElementsByClassName("text-2xl font-semibold relative display-font text-secondary")[0].textContent)){
+			//let a = document.getElementsByClassName("text-5xl font-mono relative inline-flex items-center justify-center p-6 bg-emerald-950/50 text-emerald-400")[0].textContent;
+			let b = document.getElementsByClassName("cursor-pointer text-3xl font-mono relative p-4 transition-colors duration-200 hover:bg-emerald-900/50 text-emerald-800");
+			for(var i = 0; i < 9; i+=1){
+				if(a[i]=='1' && '0'==document.getElementsByClassName("grid grid-cols-3 gap-2 w-full aspect-square mb-4 p-4 rounded-lg bg-black/20 backdrop-blur-sm border border-primary/20")[0].children[i].textContent){
+					const rect = document.getElementsByClassName("grid grid-cols-3 gap-2 w-full aspect-square mb-4 p-4 rounded-lg bg-black/20 backdrop-blur-sm border border-primary/20")[0].children[i].getBoundingClientRect();
+					const x = rect.left + rect.width / 2;
+					const y = rect.top + rect.height / 2;
+					chrome.runtime.sendMessage({ x, y });
+					break;
+				}
+			}
+		}
+		else{
+			clearInterval(kk7);
+			k1();
+		}
+	}, 1);
+}
+//打水母
+setInterval(function(){
+    if(document.getElementsByClassName("absolute items-center justify-center flex-1 w-full h-full text-black-dark ")[2]!==undefined){
+		setTimeout(function(){
+			var hp = document.getElementsByClassName("absolute items-center justify-center flex-1 w-full h-full text-black-dark ");
+			hp = hp[hp.length - 1].textContent;
+			var c = document.getElementsByClassName("relative ion-activatable cursor-pointer clickable flex-1 ");
+			if(hp.split(' / ')[1].split(',').join('') * person.recure * 0.01  >  hp.split(' / ')[0].split(',').join('')){
+				for(var i = 0; i < 3; i++){
+					if(person.fight[i]=='1'){
+						if(!/快捷欄/.test(c[i].textContent)){
+							const rect = c[i].getBoundingClientRect();
+							const x = rect.left + rect.width / 2;
+							const y = rect.top + rect.height / 2;
+							chrome.runtime.sendMessage({ x, y });
+						}
+						break;
+					}
+				}
+			}
+			else{
+				for(var i = 3; i < 6; i++){
+					if(person.fight[i]=='1'){
+						const rect = c[i].getBoundingClientRect();
+						const x = rect.left + rect.width / 2;
+						const y = rect.top + rect.height / 2;
+						chrome.runtime.sendMessage({ x, y });
+						break;
+					}
+				}
+			}
+		}, Math.floor(Math.random()*1000));
+	}
+}, 3000);
+var k = 5;
+//撿東西
+setInterval(function(){
+    if(k > 0 && document.getElementsByClassName("mobile-clean-card w-full transform origin-center transition-transform active:scale-[0.98] MobileCleanCard_card__6YGZv mb-2 p-2 pl-4 pr-4 flex-row items-center cursor-pointer mt-8 ")[0]!==undefined && document.getElementsByClassName("mobile-clean-card w-full transform origin-center transition-transform active:scale-[0.98] MobileCleanCard_card__6YGZv mb-2 p-2 pl-4 pr-4 flex-row items-center cursor-pointer mt-8 ")[0].textContent === '拿取全部物品'){
+		k -= 1;
+		setTimeout(function(){
+			const rect = document.getElementsByClassName("loading-background-front absolute top-0 left-0 w-full h-full bg-center bg-cover z-[-1] pointer-events-none")[0].getBoundingClientRect();
+			const x = rect.left + rect.width / 2;
+			const y = rect.top + rect.height / 2;
+			chrome.runtime.sendMessage({ x, y });
+		},900+Math.floor((Math.random()*200)+1));
+		if(document.getElementsByClassName("mobile-clean-card w-full transform origin-center transition-transform active:scale-[0.98] MobileCleanCard_card__6YGZv mb-2 p-2 pl-4 pr-4 flex-row items-center cursor-pointer mt-8 ")[0]!==undefined)
+			k=0;
+	}
+	else if(document.getElementsByClassName("mobile-clean-card w-full transform origin-center transition-transform active:scale-[0.98] MobileCleanCard_card__6YGZv mb-2 p-2 pl-4 pr-4 flex-row items-center cursor-pointer mt-8 ")[0]===undefined){
+		k = 5;
+	}
+}, 2000);
+//刷新
+setInterval(function(){
+	var t=document.getElementsByClassName("relative ion-activatable cursor-pointer mt-2 flex flex-row items-center border border-primary-dark flex-grow-0 pl-4 pr-4 p-1")[0];
+	if(t!==undefined&&/手動刷新/.test(t.textContent)){
+		const rect = t.getBoundingClientRect();
+		const x = rect.left + rect.width / 2;
+		const y = rect.top + rect.height / 2;
+		chrome.runtime.sendMessage({ x, y });
+	}
+	var t1=document.getElementsByClassName("text-white justify-center items-center text-center p-6");
+	if(t1[0]!=undefined&&/Uh oh, My bad.. Something went wrong/.test(t1[0].textContent)){
+		location.reload();
+	}
+}, 1000);
+var k1 = function(){
+	var kk1 = setInterval(function(){
+		if(document.getElementsByClassName("h-8 w-full next-color-box text-black-dark display-font justify-center items-center font-bold text-sm uppercase relative")[0] !== undefined){
+			clearInterval(kk1);
+			k3();
+		}
+		if(document.getElementsByClassName("mb-4 mt-2 text-center text-xl text-yellow-500 font-bold")[0] !== undefined && /神經電路覆寫/.test(document.getElementsByClassName("mb-4 mt-2 text-center text-xl text-yellow-500 font-bold")[0].textContent)){
+			clearInterval(kk1);
+			k4();
+		}
+		if(document.getElementsByClassName("text-sm opacity-80 font-mono tracking-wide")[0] !== undefined){
+			clearInterval(kk1);
+			k5();
+		}
+		if(document.getElementsByClassName("text-xl font-mono font-bold inline-block text-emerald-400")[0] !== undefined && /解密 神經數據.exe/.test(document.getElementsByClassName("text-xl font-mono font-bold inline-block text-emerald-400")[0].textContent)){
+			clearInterval(kk1);
+			k6();
+		}
+		if(document.getElementsByClassName("text-2xl font-semibold relative display-font text-secondary")[0] !== undefined && /記憶庫覆寫/.test(document.getElementsByClassName("text-2xl font-semibold relative display-font text-secondary")[0].textContent)){
+			clearInterval(kk1);
+			k7();
+		}
+	}, 1000);
+}
+k1();
+setInterval(function(){
+			//ai
+			if(person.ai=='1'){
+				if(document.getElementsByClassName("relative ion-activatable cursor-pointer clickable mb-2")[0]!==undefined && /掛機機器人/.test(document.getElementsByClassName("relative ion-activatable cursor-pointer clickable mb-2")[0].textContent)){			
+					var img=document.getElementsByClassName("display-font font-bold text-left w-full mt-1 bg-primary text-black-darker text-sm uppercase pl-2 pr-2")[0];
+					if(img==undefined||!(/將所有進度圖示拖入框框內以啟動任務。/.test(img.textContent)))
+					setTimeout(function(){
+						const rect = document.getElementsByClassName("relative ion-activatable cursor-pointer clickable mb-2")[0].getBoundingClientRect();
+						const x = rect.left + rect.width / 2;
+						const y = rect.top + rect.height / 2;
+						chrome.runtime.sendMessage({ x, y });
+						setTimeout(function(){
+							location.reload();
+						},10*60*1e+3);
+						setTimeout(function(){
+							var p=document.getElementsByClassName("mobile-clean-card w-full transform origin-center transition-transform active:scale-[0.98] MobileCleanCard_card__6YGZv mb-2 p-2 pl-4 pr-4 flex-row items-center cursor-pointer ");
+							const rect = p[p.length-1].getBoundingClientRect();
+							const x = rect.left + rect.width / 2;
+							const y = rect.top + rect.height / 2;
+							chrome.runtime.sendMessage({ x, y });
+						},400+Math.floor((Math.random()*200)+1));
+					},400+Math.floor((Math.random()*200)+1));
+				}
+			}
+			//hash
+			if(person.mining=='1'){
+				if(err4>0){
+					setTimeout(function(){
+						location.reload();
+					},10*60*1e+3);
+					/*const rect = document.getElementsByClassName("absolute w-full h-full top-0 left-0 z-10 cursor-pointer")[0].getBoundingClientRect();
+					const x = rect.left + rect.width / 2;
+					const y = rect.top + rect.height / 2;
+					chrome.runtime.sendMessage({ x, y });
+					err4 = 0;*/
+				}
+				var p = document.getElementsByClassName("mobile-clean-card w-full transform origin-center transition-transform active:scale-[0.98] MobileCleanCard_card__6YGZv mb-2 p-2 pl-4 pr-4 flex-row items-center cursor-pointer");
+				if(p.length!==0 && /開始/.test(p[p.length-1].textContent) && document.getElementsByClassName("relative ion-activatable cursor-pointer clickable mb-2")[4]!==undefined){
+					//alert(p[p.length-1].getBoundingClientRect().top+"&"+p[p.length-1].getBoundingClientRect().height);
+					const rect = p[p.length-1].getBoundingClientRect();
+					const x = rect.left + rect.width / 2;
+					const y = rect.top + rect.height / 2;
+					chrome.runtime.sendMessage({ x, y });
+					err4+=1;
+				}
+				else if(document.getElementsByClassName("relative ion-activatable cursor-pointer clickable mb-2")[4]!==undefined){
+					err4 = 0;
+					const rect = document.getElementsByClassName("relative ion-activatable cursor-pointer clickable mb-2")[4].getBoundingClientRect();
+					const x = rect.left + rect.width / 2;
+					const y = rect.top + 10;
+					chrome.runtime.sendMessage({ x, y });
+				}
+			}
+}, 1300);
+var err4 = 0;
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; domain=cybercodeonline.com";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+var person = {
+	info:"info",
+	fight:localStorage.getItem('a'),
+	recure:localStorage.getItem('b'),
+	ai:localStorage.getItem('c'),
+	mining:localStorage.getItem('d'),
+};
+/*
+alert(
+	"request.action：" + person.action + '\n' +
+	"request.fight：" + person.fight + '\n' +
+	"request.recure：" + person.recure + '\n' +
+	"request.ai：" + person.ai + '\n' +
+	"request.mining：" + person.mining
+);
+*/
+chrome.runtime.onMessage.addListener(
+	function (request, sender, sendResponse) {
+	    if (request.action == 'info') {
+	        // 赋值
+			//console.log("request.action：" + request.action);
+			//console.log("request.fight：" + request.fight);
+			//console.log("request.recure：" + request.recure);
+			//console.log("request.ai：" + request.ai);
+			//console.log("request.mining：" + request.mining);
+			/*alert(
+				"request.action：" + request.action + '\n' +
+				"request.fight：" + request.fight + '\n' +
+				"request.recure：" + request.recure + '\n' +
+				"request.ai：" + request.ai + '\n' +
+				"request.mining：" + request.mining
+			);*/
+			person = request;
+	        window.fight = request.fight;
+	        window.recure = request.recure;
+	        window.ai = request.ai;
+	        window.mining = request.mining;
+			localStorage.setItem("a", window.fight);
+			localStorage.setItem("b", window.recure);
+			localStorage.setItem("c", window.ai);
+			localStorage.setItem("d", window.mining);
+	        switch (window.fight) {
+	            case '000001':
+					//localStorage.setItem("a", "1");
+					//a='1';
+	            	// code...
+	            	// 返回数据
+                    sendResponse({
+                        result: true
+                    });
+	                break;
+				default:
+					//localStorage.setItem("a", "0");
+					//a='0';
+	            	// code...
+	            	// 返回数据
+                    sendResponse({
+                        result: true
+                    });
+	                break;
+	        }
+	    } else {
+			person.action='info';
+	        sendResponse(
+				person
+			);
+	    }
+	}
+);
---
+++ popup.js
@@ -0,0 +1,250 @@
+var start=function(){
+	if(localStorage.getItem('a')==null||localStorage.getItem('a').length!=6){
+		localStorage.setItem("a", "000000");
+	}
+	for(var i = 0; i < 6; i++){
+		if(localStorage.getItem('a')[i]=='0'){
+			document.querySelector('#fight'+i).style.borderRight='50px solid red';
+			document.querySelector('#fight'+i).style.borderLeft='';
+			document.querySelector('#fight'+i).innerText='關閉';
+		}
+		else{
+			document.querySelector('#fight'+i).style.borderRight='';
+			document.querySelector('#fight'+i).style.borderLeft='50px solid green';
+			document.querySelector('#fight'+i).innerText='開啟';
+		}
+		document.getElementById('fight'+i).addEventListener('click',function(){
+			//console.log(getCookie('a'));
+			var s = localStorage.getItem('a');
+			s = s.split('');
+			console.log(localStorage.getItem('a'));
+			if(localStorage.getItem('a')[parseInt(this.id[5])]=='1'){
+				this.style.borderRight='50px solid red';
+				this.style.borderLeft='';
+				this.innerText='關閉';
+				s[parseInt(this.id[5])] = '0';
+				s = s.join('');
+				localStorage.setItem("a", s);
+				//setCookie('a', '0', 900);
+			}
+			else{
+				for(var j = 0; j < 3; j++){
+					console.log(j);
+					s[3*parseInt(this.id[5]/3) + j] = '0';
+					document.querySelector('#fight'+(3*parseInt(this.id[5]/3) + j)).style.borderRight='50px solid red';
+					document.querySelector('#fight'+(3*parseInt(this.id[5]/3) + j)).style.borderLeft='';
+					document.querySelector('#fight'+(3*parseInt(this.id[5]/3) + j)).innerText='關閉';
+				}
+				this.style.borderRight='';
+				this.style.borderLeft='50px solid green';
+				this.innerText='開啟';
+				s[parseInt(this.id[5])] = '1';
+				s = s.join('');
+				localStorage.setItem("a", s);
+				//setCookie('a', '1', 900);
+			}
+			info.fight = localStorage.getItem('a');
+			// 获取具有指定属性的所有标签页，active: true 标签页在窗口中是否为活动标签页；currentWindow 标签页是否在当前窗口中。
+			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+				// 取出当前标签页的 tag_id, 发送一个消息出去, 同时带上回调函数
+				chrome.tabs.sendMessage(tabs[0].id, info, function (response) {
+					// 回调函数(传回的信息)
+					if (response.result) {
+						// 关闭 popup.html 页面
+						//window.close();
+						console.log(response.result);
+					} else {
+						console.log(response.note);
+					}
+				});
+			});
+		});
+	}
+}
+start();
+if(localStorage.getItem('c')=='0'){
+	document.querySelector('#mining').style.borderRight='50px solid red';
+	document.querySelector('#mining').style.borderLeft='';
+	document.querySelector("#mining").innerText='關閉';
+}
+else{
+	document.querySelector('#mining').style.borderRight='';
+	document.querySelector('#mining').style.borderLeft='50px solid green';
+	document.querySelector("#mining").innerText='開啟';
+}if(localStorage.getItem('d')=='0'){
+	document.querySelector('#ai').style.borderRight='50px solid red';
+	document.querySelector('#ai').style.borderLeft='';
+	document.querySelector("#ai").innerText='關閉';
+}
+else{
+	document.querySelector('#ai').style.borderRight='';
+	document.querySelector('#ai').style.borderLeft='50px solid green';
+	document.querySelector("#ai").innerText='開啟';
+}
+document.getElementById('valuebar').value = localStorage.getItem('b');
+document.getElementById('valuebar1').value = localStorage.getItem('b');
+for(var i of document.getElementsByTagName('span')){
+	i.style.font='bold 18px sans-serif';
+}
+for(var i of document.getElementsByTagName('button')){
+	i.style.width='100px';
+}
+document.getElementById('ai').addEventListener('click',function(){
+	if(localStorage.getItem('d')=='1'){
+		document.querySelector('#ai').style.borderRight='50px solid red';
+		document.querySelector('#ai').style.borderLeft='';
+		document.querySelector("#ai").innerText='關閉';
+		localStorage.setItem("d", "0");
+	}
+	else{
+		document.querySelector('#ai').style.borderRight='';
+		document.querySelector('#ai').style.borderLeft='50px solid green';
+		document.querySelector("#ai").innerText='開啟';
+		localStorage.setItem("d", "1");
+	}
+	info.ai = localStorage.getItem('d');
+	// 获取具有指定属性的所有标签页，active: true 标签页在窗口中是否为活动标签页；currentWindow 标签页是否在当前窗口中。
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		// 取出当前标签页的 tag_id, 发送一个消息出去, 同时带上回调函数
+		chrome.tabs.sendMessage(tabs[0].id, info, function (response) {
+			// 回调函数(传回的信息)
+			if (response.result) {
+				// 关闭 popup.html 页面
+				//window.close();
+				console.log(response.result);
+			} else {
+				console.log(response.note);
+			}
+		});
+	});
+});
+document.getElementById('mining').addEventListener('click',function(){
+	if(localStorage.getItem('c')=='1'){
+		document.querySelector('#mining').style.borderRight='50px solid red';
+		document.querySelector('#mining').style.borderLeft='';
+		document.querySelector("#mining").innerText='關閉';
+		localStorage.setItem("c", "0");
+	}
+	else{
+		document.querySelector('#mining').style.borderRight='';
+		document.querySelector('#mining').style.borderLeft='50px solid green';
+		document.querySelector("#mining").innerText='開啟';
+		localStorage.setItem("c", "1");
+	}
+	info.mining = localStorage.getItem('c');
+	// 获取具有指定属性的所有标签页，active: true 标签页在窗口中是否为活动标签页；currentWindow 标签页是否在当前窗口中。
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		// 取出当前标签页的 tag_id, 发送一个消息出去, 同时带上回调函数
+		chrome.tabs.sendMessage(tabs[0].id, info, function (response) {
+			// 回调函数(传回的信息)
+			if (response.result) {
+				// 关闭 popup.html 页面
+				//window.close();
+				console.log(response.result);
+			} else {
+				console.log(response.note);
+			}
+		});
+	});
+});
+// 获取具有指定属性的所有标签页，active: true 标签页在窗口中是否为活动标签页；currentWindow 标签页是否在当前窗口中。
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+	// 取出当前标签页的 tag_id, 发送一个消息出去, 同时带上回调函数
+	info.action='info';
+	chrome.tabs.sendMessage(tabs[0].id, info, function (response) {
+		// 回调函数(传回的信息)
+		if (response.result) {
+			// 关闭 popup.html 页面
+			//window.close();
+			console.log(response.result);
+		} else {
+			info = response;
+			document.getElementById('valuebar').value=info.recure;
+			document.getElementById('valuebar1').value=info.recure;
+			console.log(info);
+		}
+	});
+});
+var info = {action:"info",fight:localStorage.getItem('a'), recure:localStorage.getItem('b'), mining:localStorage.getItem('c'), ai:localStorage.getItem('d')};
+document.getElementById('valuebar').addEventListener('input',function(){//拉條
+	document.getElementById('valuebar1').value=this.value;
+	localStorage.setItem("b", this.value);
+	info.recure=localStorage.getItem('b');
+	// 获取具有指定属性的所有标签页，active: true 标签页在窗口中是否为活动标签页；currentWindow 标签页是否在当前窗口中。
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		// 取出当前标签页的 tag_id, 发送一个消息出去, 同时带上回调函数
+		chrome.tabs.sendMessage(tabs[0].id, info, function (response) {
+			// 回调函数(传回的信息)
+			if (response.result) {
+				// 关闭 popup.html 页面
+				//window.close();
+				console.log(response.result);
+			} else {
+				console.log(response.note);
+			}
+		});
+	});
+});
+document.getElementById('valuebar').addEventListener('focusout',function(){
+});
+document.getElementById('valuebar1').addEventListener('focusout',function(){//箱輸入失焦
+	console.log('unfocus');
+	var c = this.value;
+	if(c=='') c=0;
+	c = parseFloat(c);
+	c = Math.max(c,0);
+	c = Math.min(c,100);
+	if(c==NaN) c=0;
+	console.log(c==NaN);
+	document.getElementById('valuebar').value = c;
+	document.getElementById('valuebar1').value = c;
+	localStorage.setItem("b", document.getElementById('valuebar').value);
+	info.recure=localStorage.getItem('b');
+	// 获取具有指定属性的所有标签页，active: true 标签页在窗口中是否为活动标签页；currentWindow 标签页是否在当前窗口中。
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		// 取出当前标签页的 tag_id, 发送一个消息出去, 同时带上回调函数
+		chrome.tabs.sendMessage(tabs[0].id, info, function (response) {
+			// 回调函数(传回的信息)
+			if (response.result) {
+				// 关闭 popup.html 页面
+				//window.close();
+				console.log(response.result);
+			} else {
+				console.log(response.note);
+			}
+		});
+	});
+});
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
